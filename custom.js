af1aaca6089f1deeeec37a63518e30f2370cdd64
Md. Jawad Jamil <md.jawadjamil@gmail.com>
2021-08-06 16:51:47 +0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -4,7 +4,7 @@ let pics = [
 "gifs/gif3.gif",
 ]
 
-let imgTag = document.querySelector('img');
+let imgTag = document.querySelector('#gif_img');
 
 let number = 0;
 
@@ -21,8 +21,6 @@ function next(){
 
 setInterval(next,4000)
 
-console.log(pics);
-
 function prev() {
     number--
 
@@ -31,4 +29,77 @@ function prev() {
     }
 
     imgTag.src = pics[number];
+}
+
+
+let all_video = [
+    "videos/video1.mp4",
+    "videos/video2.mp4",
+    "videos/video3.mp4",
+    "videos/video4.mp4",
+    "videos/video5.mp4",
+    "videos/video6.mp4",
+    "videos/video7.mp4",
+    "videos/video8.mp4",
+    "videos/video9.mp4",
+]
+
+
+let VideoTag = document.querySelector('#myVid');
+
+let count = 0;
+
+function nextVideo(){
+    count++;
+
+    if (count>=all_video.length) {
+        count = 0;
+    }
+
+    VideoTag.src = all_video[count];
+}
+
+function prevVideo() {
+    count--
+
+    if (count<0) {
+        count = all_video.length-1;
+    }
+
+    VideoTag.src = all_video[count];
+}
+
+
+
+let MyVideo = document.getElementById("myVid")
+let poster = document.querySelector(".overly_poster")
+let play_btn = document.getElementById("playPause_btn")
+
+function playPause() {
+    if (MyVideo.paused) {
+        MyVideo.play()
+        poster.setAttribute("style", "visibility: hidden; transition:.2s;");
+        play_btn.classList.remove('fa-play-circle');
+        play_btn.classList.add('fa-pause-circle');
+        play_btn.setAttribute("style", "padding-top: 50px; transition:.2s; opacity:0;")
+        
+    } else if (MyVideo.played) {
+        MyVideo.pause()
+        poster.style.display = "inline-block";
+        poster.setAttribute("style", "visibility: visible; transition:.2s;");
+        play_btn.classList.remove('fa-pause-circle');
+        play_btn.classList.add('fa-play-circle');
+        play_btn.setAttribute("style", "padding-top: 0px; transition:.2s; opacity:1;")
+    }
+
+}
+
+// function display_overly() {
+//     poster.setAttribute("style", "visibility: visible; transition:.2s;");
+//     play_btn.setAttribute("style", "padding-top: 0px; transition:.2s; opacity:1;")
+// }
+
+function hiding() {
+    poster.setAttribute("style", "visibility: hidden; transition:.2s;");
+    play_btn.setAttribute("style", "opacity:0;")
 }
